b91310f4e8852a2a9bff1f039be621ee04391d0a
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-06 23:48:22 +0530

    basic

---
+++ clg_js/index.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+// console.error("error");
+// console.warn("byee");
+// console.log(5-"5")// 0 aayega q ki - number me convert krke tb operate krta h
+// console.log(5+'5')// 55 ayega qki as a string trate hoga + operatr
+// console.log(window)// global object
+let arr=[1,2,3,'hii',true]
+// for(let a in arr){
+//     console.log(a); // for in loop by default array ka index number print krta h 
+
+// }
+for(let a of arr){
+    console.log(a);// for of loop value return krta h 
+
+}
---
+++ clg_js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for(let a in arr){
+//     console.log(a); // for in loop by default array ka index number print krta h 
+
+// }
