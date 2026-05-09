ba242c66abc6a5b6402e6ecbb066c2dcdb14cc73
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-10 05:13:14 +0530

    operators

---
+++ lecture2/operator/2.js
@@ -0,0 +1,6 @@
+// comparision operator
+
+console.log("5"==5)
+// ye loose eqality jo ki keval value ko compare krta h isliye output true aayega 
+console.log("5"===5)
+// ye strict eqality jo data type bhi compare krta h 
