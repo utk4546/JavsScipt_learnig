46ba2aac57215089bb46a6cf3d9d0ce981032c16
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-11 04:41:48 +0530

    array&methods

---
+++ clg_js/array/methods/index1.js
@@ -0,0 +1,19 @@
+
+
+// push opreration on array
+// value end me add hoti h 
+
+let nums = [1,2,3];
+console.log("starting array",nums);
+
+nums.push(4);
+
+console.log("after push methods",nums);
+
+nums.unshift(0) //isse element starting se add me add hota h 
+console.log("after unshift method",nums);
+nums.shift(4)//isse element starting se remove hota h
+console.log("after shift method",nums);
+nums.pop(4)// isse element end se remove hoti h 
+console.log("after pop method",nums);
+
---
+++ clg_js/operator/lec3.js
@@ -0,0 +1,14 @@
+let fruits = ["apple", "banana", "mango"];
+
+console.log(fruits);
+
+
+// indexing
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+
+
+// length
+
+console.log(fruits.length);
