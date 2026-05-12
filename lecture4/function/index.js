dde4e8dfcb4906bad9068841d43fc9cab43a6ff6
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-13 01:56:52 +0530

    function

---
+++ lecture4/function/index.js
@@ -0,0 +1,16 @@
+// console.log("utk")
+
+// function name(){
+//     console.log("utkarsh tiwari")
+// }
+
+// name();
+
+
+function printCount(){
+    for(let i=1; i<=100;i++){
+        console.log(i);
+    }
+}
+
+printCount();
