d723cad9f29a926bc366401d480467ef49384876
Utkarsh Tiwari <91utkarsh@gmail.com>
2026-05-13 02:09:07 +0530

    function

---
+++ lecture4/function/index2.js
@@ -0,0 +1,24 @@
+// function printCount(n){
+//     for(let i=1; i<=n;i++){
+//         console.log(i);
+//     }
+// }
+
+// printCount(10);
+
+
+// function avrg(num1,num2){
+//     avg=(num1+num2)/2
+//     console.log(avg)
+// }
+
+// avrg(5,8)
+
+
+function printsum(a,b,c){
+    sum=a+b+c
+    return sum
+}
+
+let p=printsum(5,8,7)
+console.log(p)
